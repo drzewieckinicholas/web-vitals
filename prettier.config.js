d5f9de75e017060e3a3db45af863ee3ac41612d7
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2024-01-25 17:51:38 -0800

    feat: implement Tailwind typography plugin

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+export default {
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  jsxSingleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
