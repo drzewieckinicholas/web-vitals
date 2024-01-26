730604ef05a7ee09c1d98409d55d42ab13474099
Nicholas Drzewiecki <drzewieckinicholas@gmail.com>
2024-01-25 21:58:52 -0800

    feat: add a CLS trigger on the CLS page

--- src/routes/CumulativeLayoutShift/index.tsx
+++ src/routes/CumulativeLayoutShift/index.tsx
@@ -1,12 +1,26 @@
-export default function CumulativeLayoutShift() {
+import { useEffect, useState } from 'react';
+
+export default function CumulativeLayoutShift(): JSX.Element {
+  const [shiftElement, setShiftElement] = useState<boolean>(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShiftElement(true);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <>
       <header>
         <h1>Cumulative Layout Shift</h1>
         <p>Lorem ipsum dolor sit amet.</p>
       </header>
-      <section>
-        <h2>Heading</h2>
+      <section className={shiftElement ? 'mt-80' : undefined}>
+        <h2>Layout Shifts</h2>
         <p>Lorem ipsum dolor sit amet.</p>
       </section>
     </>
